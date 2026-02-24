35703db0a22b4194f957bde20199568ff81d8eba
luismgm3 <luismgm3@gmail.com>
2026-02-24 16:17:53 +0000

    Initial commit. Project + Database start up.

---
+++ amq-backend/prisma/migrations/20260224160332_init/migration.sql
@@ -0,0 +1,49 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "isVerified" BOOLEAN NOT NULL DEFAULT false,
+    "verifyToken" TEXT,
+    "malAccessToken" TEXT,
+    "malRefreshToken" TEXT,
+    "malUsername" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Game" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "score" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Game_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Round" (
+    "id" SERIAL NOT NULL,
+    "gameId" INTEGER NOT NULL,
+    "animeId" INTEGER NOT NULL,
+    "animeTitle" TEXT NOT NULL,
+    "openingTitle" TEXT NOT NULL,
+    "correct" BOOLEAN NOT NULL,
+
+    CONSTRAINT "Round_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Game" ADD CONSTRAINT "Game_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Round" ADD CONSTRAINT "Round_gameId_fkey" FOREIGN KEY ("gameId") REFERENCES "Game"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
