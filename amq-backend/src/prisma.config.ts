0937333964cc11f359a93868d4cb8a7046412eda
luismgm3 <luismgm3@gmail.com>
2026-02-24 16:24:09 +0000

    Created Prisma's migration

--- amq-backend/src/prisma.config.ts
+++ amq-backend/src/prisma.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'prisma/config'
 import 'dotenv/config'
 
 export default defineConfig({
+    schema: 'prisma/schema.json',
     datasource: {
         url: process.env.DATABASE_URL,
     },
